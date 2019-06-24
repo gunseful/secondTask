665383d7390468bd68a2ca6ff4441299ba1de219
Alexander Bocharnikov <balexander1995@gmail.com>
2019-06-24 22:03:07 +0300

    Added tests. Moved to Java 8. Refactored MAIN.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,19 @@ import readers.Reader;
 import java.io.IOException;
 
 public class Main {
+
+    private static final Reader READER = new Reader();
+
     public static void main(String[] args) throws IOException {
+        String sourceText = READER.read("src/text.txt");
 
-        Reader reader = new Reader();
-
-        Words words = new Words(reader.read("src/text.txt"));
-        words.getItems(); // Получаем коллекцию слов из текста
-        words.backToText(words.getItems());     // Собираем слова обратно в текст
+        Words words = new Words(sourceText);
+        String fromWords = words.backToText(words.getItems());// Собираем слова обратно в текст
+        if (!fromWords.equals(sourceText)) throw new RuntimeException("Text is not equal");
         // words.alphabeticalSortingPrint();  //Сразу выводит в консоль все слова из текста в алфавитном порядке без повторений
-        Paragraphs parapraphs = new Paragraphs(reader.read("src/text.txt"));
-        parapraphs.getItems(); // Получаем коллекцию параграфов из текста
-        parapraphs.backToText(parapraphs.getItems()); // Собираем параграфы обратно в текст
-
+        Paragraphs parapraphs = new Paragraphs(sourceText);
+        String fromParagraph = parapraphs.backToText(parapraphs.getItems());// Собираем параграфы обратно в текст
+        if (!fromParagraph.equals(sourceText)) throw new RuntimeException("Text is not equal");
         // C классами Sententence и Symbols можно проделать тоже самое что и с теми примерами что сверху
     }
 }
